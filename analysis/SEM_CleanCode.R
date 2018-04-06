3b1cc521e4dbc52e8071561953fab8503faa1f99
Wilson-Harmon <r74t532@msu.montana.edu>
2018-04-06 16:12:41 -0600

    mas chanegeados have been made-o... yeah, I took five years of spanish

--- analysis/SEM_CleanCode.R
+++ analysis/SEM_CleanCode.R
@@ -33,6 +33,7 @@ colnames(cc2015percap) <- c("PDNRSTAFF_PC", "S.ER.D_PC", "NONS.ER.D_PC")
 
 cc2015percap.r<-data.frame(sapply(cc2015percap,minrank))
 cc2015_r <- cbind(cc2015.r, cc2015percap.r)
+#RUN THIS FOR CORRPLOT: (commented out otherwise)
 names(cc2015_r) <- c("Name","2010 Basic","2015 Basic","Faculty Size", "Hum PhD",
                      "Other PhD","Soc. Sci PhD", "STEM PhD","Res. Staff", "STEM Exp", 
                      "NS Exp", "Res. Staff PC", "STEM Exp PC  ", "NS Exp PC")
@@ -41,7 +42,7 @@ cc2015_matrix2 <- as.matrix(cc2015_r[-c(1:3)])
 corrmatrix <- Hmisc::rcorr(cc2015_matrix2)
 
 par(mar = c(1,.8,.8,2))
-corrplot::corrplot(corrmatrix$r, order="hclust", type = "upper")
+corrplot::corrplot(corrmatrix$r, order="hclust", type = "upper", method = 'number')
 text(-4,3, 'Correlation Plot', font = 2)
 text(-4,2,'of Manifest Variables', font = 2)
 rect(-7,1.3,-1,3.5)
@@ -51,11 +52,23 @@ HUMANITIES=~HUM_RSD+OTHER_RSD+SOCSC_RSD+NONS.ER.D+FACNUM
 STEM=~STEM_RSD+PDNFRSTAFF+S.ER.D+FACNUM
 Aggregate=~HUMANITIES+STEM'
 
+model4 <- '
+non-STEM = ~ Hum PhD + Other PhD + Soc. Sci PhD + 
+STEM
+'
+
+
 lavaan_sem_new <- lavaan::sem(model4, data=cc2015_r, std.lv=TRUE,
                               orthogonal=FALSE, se="robust.huber.white")
 lavaan::summary(lavaan_sem_new, standardized=TRUE, fit.measures=TRUE)
 CCScores <- as.data.frame(lavaan::predict(lavaan_sem_new))
 rownames(CCScores) <- cc2015Ps$NAME
+
+#path diagram
+
+source('https://raw.githubusercontent.com/brandmaier/onyxR/master/tools/install.R')
+library(onyxR)
+onyx(lavaan_sem_new)
 
 library(beanplot)
 beanplot(CCScores$Aggregate,log="",col="yellow",method="jitter")
